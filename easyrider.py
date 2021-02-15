66c3df2c01dff84941126bffe24900f7d4049a40
Eugene <eugenekh@outlook.com>
2021-02-15 13:05:10 +0500

    stage_3 after clearing code

--- easyrider.py
+++ easyrider.py
@@ -1,23 +1,5 @@
 import json
-import jsonschema
-from schema import schema, fileds
 from collections import defaultdict
-
-
-def json_validate():
-    v = jsonschema.Draft7Validator(schema)
-    for m in json_in:
-        errors = v.iter_errors(m)
-        for error in errors:
-            filed_id = error.absolute_path[0]
-            fileds[filed_id] += 1
-
-
-def print_errors():
-    print(f"Type and required field validation: {sum(fileds.values())} errors")
-    for k, v in fileds.items():
-        print(f"{k}: {v}")
-
 
 json_in = json.loads(input())
 line_stops = defaultdict(int)
@@ -26,4 +8,3 @@ for track in json_in:
 
 for line, stops in line_stops.items():
     print(f"bus_id: {line}, stops: {stops}")
-
